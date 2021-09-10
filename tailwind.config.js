9216b04aacfa2a469321ce215c0ce0ef43338db9
Justin Morgan <justin.morgan@hey.com>
2021-09-09 17:12:48 -0700

    :heavy_plus_sign: (tailwind) Integrate tailwind per tw docs

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Spartan', 'sans-serif'],
+      serif: ['serif']
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'header-desktop': "url('images/bg-header-desktop.svg')",
+        'header-mobile': "url('images/bg-header-mobile.svg')"
+      }),
+      colors: {
+        primary: {
+          dark: "hsl(180, 29%, 50%)"
+        },
+        neutral: {
+          faint: "hsl(180, 31%, 95%)",
+          light: "hsl(180, 52%, 96%)",
+          medium:  "hsl(180, 8%, 52%)",
+          dark: "hsl(180, 14%, 20%)"
+        }
+      },
+      boxShadow: {
+        primaryGlow: "0 10px 30px hsla(180, 29%, 50%, 0.5)"
+      }
+    },
+  variants: {
+    extend: {},
+  },
+  plugins: []
+  }
+}
